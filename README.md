d574362f477fc9045bca9bdaf88e4e364eb79940
Alok Singh <alokbeniwal@gmail.com>
2023-03-29 10:55:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+An idea I had listening around conversations at my birthday: can I compute the vibe of a room? What does that even mean? The idea to use a Hodge decomposition immediately came.
+
+I'm curious to see what will happen.
+
+Idea: Split 1 audio file into 3, corresponding to the 3 pieces of the Hodge decomposition. The harmonic part is the "vibe" of the room. The other two are because I'm curious and they came for free. There are 3 more files, corresponding to the pairwise combos, which are
+
+1.  the "vibe" of the room
+2.  the curl of the room
+3.  the divergence of the room
+
+TODO elaborate hodge and how helmholtz works too in this case.
+
---
+++ vibe_of_a_room/__init__.py
@@ -0,0 +1,80 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import torch
+import torchaudio
+from scipy.signal import convolve
+from torchaudio.transforms import Spectrogram
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Perform Helmholtz decomposition on the spectrogram of an audio file."
+    )
+    parser.add_argument("input_file", type=Path, help="Path to the input audio file.")
+    parser.add_argument(
+        "output_directory", type=Path, help="Path to the output directory."
+    )
+    args = parser.parse_args()
+
+    waveform, SAMPLE_RATE = torchaudio.load(args.input_file)
+
+    # Compute the spectrogram
+    spectrogram = Spectrogram()(waveform).squeeze().numpy()
+
+    # Perform Helmholtz decomposition on the spectrogram
+    curl_free, div_free = helmholtz_decomposition(spectrogram)
+    # TODO include the harmonic component
+
+    # Convert the decomposed spectrograms back to waveforms
+    curl_free_waveform = spectrogram_to_waveform(
+        curl_free, waveform.shape[0], SAMPLE_RATE
+    )
+    div_free_waveform = spectrogram_to_waveform(
+        div_free, waveform.shape[0], SAMPLE_RATE
+    )
+
+    # Save the resulting files
+    torchaudio.save(
+        args.output_directory / "curl_free.wav", curl_free_waveform, SAMPLE_RATE
+    )
+    torchaudio.save(
+        args.output_directory / "div_free.wav", div_free_waveform, SAMPLE_RATE
+    )
+
+
+def helmholtz_decomposition(spectrogram):
+    # TODO verify time and frequency axis
+    # Calculate the gradients along time (axis=1) and frequency (axis=0)
+    # Bug? The gradients are flipped
+    grad_t, grad_f = np.gradient(spectrogram)
+
+    # Calculate the Laplacians along time and frequency
+    # TODO axis
+    d2_t = np.gradient(grad_t, axis=1)
+    d2_f = np.gradient(grad_f, axis=0)
+
+    # Create a low-pass filter
+    lp_filter = np.ones((5, 5)) / 25
+
+    # Apply low-pass filter to the Laplacians to obtain the curl-free component
+    curl_free = convolve(d2_t + d2_f, lp_filter, mode="same")
+
+    # Subtract the curl-free component from the original spectrogram to obtain the divergence-free component
+    div_free = spectrogram - curl_free
+
+    return curl_free, div_free
+
+
+def spectrogram_to_waveform(spectrogram, num_channels):
+    istft_transform = torchaudio.transforms.InverseSpectrogram(
+        n_fft=400,
+        n_hop=200,
+    )
+    waveform = istft_transform(torch.tensor(spectrogram).unsqueeze(0)).clamp_(-1, 1)
+    return waveform[:num_channels]
+
+
+if __name__ == "__main__":
+    main()
